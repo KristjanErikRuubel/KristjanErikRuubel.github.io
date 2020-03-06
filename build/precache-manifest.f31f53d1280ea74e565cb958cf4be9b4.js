b5871091c5da1d1f078913f5e0a5d23c35d1438c
unknown <kristjan-e@hotmail.com>
2020-03-06 15:43:47 +0200

    initial

---
+++ build/precache-manifest.f31f53d1280ea74e565cb958cf4be9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05f34b05c01954006ed39327beaf24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b67ac564c4a39ad96cbd",
+    "url": "/static/css/main.eb602f01.chunk.css"
+  },
+  {
+    "revision": "52a117c3a60dbcf38301",
+    "url": "/static/js/2.29e50fdb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.29e50fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b67ac564c4a39ad96cbd",
+    "url": "/static/js/main.087d3ac7.chunk.js"
+  },
+  {
+    "revision": "f6ee6e01fda15e9a3e39",
+    "url": "/static/js/runtime-main.c44faf99.js"
+  }
+]);
